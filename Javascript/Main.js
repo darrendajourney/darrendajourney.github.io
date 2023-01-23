5700131203fcfd3b543df4de31cda0a6691fb08c
dajourney <darrendajourney@gmail.com>
2023-01-23 18:46:04 +0000

    files

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -14,108 +14,110 @@ const canvas = document.getElementById("renderCanvas");
         
             const scene = new BABYLON.Scene(engine);
 
-            const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2, 4, BABYLON.Vector3(0, 0, 0));
+            const camera = new BABYLON.ArcRotateCamera("Camera", -1.5708, 1.309, 10, BABYLON.Vector3(0, 0, 0));
+            camera.lowerRadiusLimit = 5;
+            camera.upperRadiusLimit = 20;
             camera.attachControl(canvas, true);
 
             scene.clearColor = new BABYLON.Color3(0, 0, 0);
 
             BABYLON.Engine.audioEngine.setGlobalVolume(1);
 
+            BABYLON.SceneLoader.ImportMesh(["mesh01"], "mesh/", "Speaker.gltf", scene, function (mesh) {
+                camera.target = mesh[0];
+                mesh[0].scaling = new BABYLON.Vector3(0.5, 0.5, 0.5);
+                mesh[0].position.z -= 0.8;
+            })
+
+           
+
             const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(-10, 30, 0), scene);
             light.diffuse = new BABYLON.Color3(0.6, 0.6, 0.6);
             light.intensity = 0.8;
 
-
-            //creating the ground
-            const ground = BABYLON.MeshBuilder.CreateGroundFromHeightMap("gdhm", "Textures/Face.JPG", {width:30, height :30, subdivisions: 60, maxHeight: 0.3});
-            ground.position.y -= 2.5;
-            ground.position.z += 3;
-
-            const groundmaterial = new BABYLON.StandardMaterial('mat', scene);
-            groundmaterial.diffuseColor = new BABYLON.Color3(1, 1, 1);
-            groundmaterial.diffuseTexture = new BABYLON.Texture("Textures/Face.JPG", scene);
-            groundmaterial.specularColor = new BABYLON.Color3(0.1, 0.1, 0.1);
-            ground.material = groundmaterial;
-
-            
+            //creating a sphere for the sound and hidding it with opacity
+            const midSphere = BABYLON.MeshBuilder.CreateSphere("sphere", {segments: 10, diameter: 5}, scene);
+            const spheremat = new BABYLON.StandardMaterial("spheremat", scene);
+            spheremat.alpha = 0.5;
+            midSphere.material = spheremat;
+            midSphere.position.y = 0.5;
 
             //creating a stop box button for the audio
             const stopBox = BABYLON.MeshBuilder.CreateBox("stopbox", {size: 1, height: 1, width: 1, depth: 0.5});
             //material for the stopbox
             stopBox.position.x += 1;
             stopBox.position.y -= 1;
-            stopBox.position.z += 2;
+            stopBox.position.z -= 3;
             //creating material for stopbox
             const boxMaterial = new BABYLON.StandardMaterial("boxmat", scene);
             boxMaterial.diffuseColor = new BABYLON.Color3(1 ,0 ,0);
-            boxMaterial.alpha = 0.6;
             stopBox.material = boxMaterial;
 
             //creating a play polyhedron for the audio
             const playPoly = BABYLON.MeshBuilder.CreatePolyhedron("playpolly", {type: 0, size: 0.1, sizeX: 0.5, sizeY: 0.5, sizeZ: 0.2});
             playPoly.position.x -= 1;
             playPoly.position.y -= 1;
-            playPoly.position.z += 2;
+            playPoly.position.z -= 3;
             //creating material for polly
             //const pollyMaterial = new BABYLON.StandardMaterial("pollymat", scene);
             const pbrPollyMat = new BABYLON.PBRMaterial("pbrmat", scene);
             playPoly.material = pbrPollyMat;
             pbrPollyMat.albedoColor = new BABYLON.Color3(0, 1, 0);
 
-            pbrPollyMat.metallic = 0.5;
+            pbrPollyMat.metallic = 0.9;
             pbrPollyMat.roughness = 0.1;
-            pbrPollyMat.alpha = 0.8;
             pbrPollyMat.subSurface.isRefractionEnables = true;
-
-
-            let radialSegments = Math.floor(Math.random() * 800 + 100);
-            let p = Math.floor(Math.random() * 160 + 100);
-            //creating a tour knot
-            const torus_knot = BABYLON.MeshBuilder.CreateTorusKnot("torusknot", {radius: 1, tube: 0.07, radialSegments, 
-                p, q: -165, updatable: true});           
-                
-            //material for the torus using 
-            let material = new BABYLON.StandardMaterial("material", scene);
-            material.diffuseColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-            material.alpha = 0.4;
-            torus_knot.material = material;
             
-            //position of torus on z 
-            torus_knot.position.z += 4;
-           
+            const SPS = new BABYLON.SolidParticleSystem("SPS", scene);
+            const sphere = BABYLON.MeshBuilder.CreateSphere("s", {});
+            const poly = BABYLON.MeshBuilder.CreatePolyhedron("p", { type: 4 });
+            SPS.addShape(sphere, 20); // 20 spheres
+            SPS.addShape(poly, 50); // 120 polyhedrons
+            sphere.dispose(); //dispose of original model sphere
+            poly.dispose(); //dispose of original model poly
+        
+            const mesh = SPS.buildMesh(); // finally builds and displays the SPS mesh
+            
+            // initiate particles function
+            SPS.initParticles = () => {
+                for (let p = 0; p < SPS.nbParticles; p++) {
+                    const particle = SPS.particles[p];
+                    particle.position.x = BABYLON.Scalar.RandomRange(-20, 20);
+                    particle.position.y = BABYLON.Scalar.RandomRange(-20, 20);
+                    particle.position.z = BABYLON.Scalar.RandomRange(-20, 20);
+                    particle.color = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+                    particle.scaling.x = Math.random() * 1 + 0.05;
+                    particle.scaling.y = Math.random() * 1 + 0.05;
+                    particle.scaling.z = Math.random() * 1 + 0.05;
+                }
+            };
+        
+            //Update SPS mesh
+            SPS.initParticles();
+            SPS.setParticles();
 
-            //rotating the torus on the y axis using a vextor 3 and rotating in world space
-            let axis = new BABYLON.Vector3(0, 0.1, 0);
-            //speed of rotation
-            let angle = 0.02;
-            //function to animate the torus
-            scene.registerAfterRender(function() {
-                torus_knot.rotate(axis, angle, BABYLON.Space.WORLD);
-
-            //creating a basic shader for the torusknot
-        let toruscolors = torus_knot.getVerticesData(BABYLON.VertexBuffer.ColorKind);
-        if(!toruscolors) {
-            toruscolors = [];
-
-            let positions = torus_knot.getVerticesData(BABYLON.VertexBuffer.PositionKind);
-
-            for(let p = 0; p < positions.length / 2; p++) {
-                toruscolors.push(Math.random(), Math.random(), Math.random());
+                //creating a basic shader for the particles
+                let toruscolors = midSphere.getVerticesData(BABYLON.VertexBuffer.ColorKind);
+                if(!toruscolors) {
+                toruscolors = [];
+    
+                let positions = midSphere.getVerticesData(BABYLON.VertexBuffer.PositionKind);
+    
+                for(let p = 0; p < positions.length / 2; p++) {
+                    toruscolors.push(Math.random(), Math.random(), Math.random());
+                }
             }
-        }
+                
+                midSphere.setVerticesData(BABYLON.VertexBuffer.ColorKind, toruscolors);
             
-            torus_knot.setVerticesData(BABYLON.VertexBuffer.ColorKind, toruscolors);
-            
-              
-            });
 
             //loading the sound track
             sounds = new BABYLON.Sound("mainmenu", "./Sounds/MainMenu.wav", scene, null, {
                 spatialSound: true,
                 distanceModel: "linear",
-                maxDistance: 30,
+                maxDistance: 20,
             });
-            sounds.attachToMesh(torus_knot);
+            sounds.attachToMesh(midSphere);
 
               //making the playpolly clickable to play some music
               playPoly.actionManager = new BABYLON.ActionManager(scene);
@@ -135,25 +137,21 @@ const canvas = document.getElementById("renderCanvas");
                       ));  
             
             //creating an audio analyser
-            const musicAnalyser = new BABYLON.Analyser(scene);
-            BABYLON.Engine.audioEngine.connectToAnalyser(musicAnalyser);
+            //const musicAnalyser = new BABYLON.Analyser(scene);
+            //BABYLON.Engine.audioEngine.connectToAnalyser(musicAnalyser);
             //musicAnalyser.drawDebugCanvas();
-            musicAnalyser.FFT_SIZE = 32;
-	        musicAnalyser.SMOOTHING = 0.3;
+            //musicAnalyser.FFT_SIZE = 32;
+	        //musicAnalyser.SMOOTHING = 0.3;
 
-            let musicFrequency = musicAnalyser.getByteFrequencyData();
-            let binCount = musicAnalyser.getFrequencyBinCount;
+            //let musicFrequency = musicAnalyser.getByteFrequencyData();
+            //let binCount = musicAnalyser.getFrequencyBinCount;
 
-            for (let i = 0; i < musicFrequency.length; i++){
-                let v = binCount[i] /= Math.pow(10, 4);
-                    v = material.diffuseColor;
-                }
-
-            setInterval(shape, 5000);
-                function shape(){
-                    material.diffuseColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-                    material.alpha = Math.random() * 0.8 + 0.1;
-                }
+            //for (let i = 0; i < musicFrequency.length; i++){
+                //let v = binCount[i] /= Math.pow(10, 4);
+                    //v = toruscolors;
+                    //console.log(toruscolors);
+                //}
+            
 
         //creating a AR session
             const xr = await scene.createDefaultXRExperienceAsync({
